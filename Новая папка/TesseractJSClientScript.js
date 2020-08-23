c15e2dc7340a03a0343fd205b3c2103cec5d442d
Mezga Alexandr <mezga.studio@gmail.com>
2020-08-23 16:10:31 +0300

    Print Version

---
+++ Новая папка/TesseractJSClientScript.js
@@ -0,0 +1,17 @@
+function onChange(control, oldValue, newValue, isLoading, isTemplate) {
+   if (isLoading || newValue === '') {
+      return;
+   }
+
+    ScriptLoader.getScripts('OCR.jsdbx', function() {
+        Tesseract.recognize('https://dev103823.service-now.com/' + g_form.getValue('name'), newValue, {
+            logger: function logger(m) {
+                return g_form.setValue('u_ocr_text', 'Progress: ' +  ((parseFloat(m['progress']) * 100).toFixed(2)) + '%');
+            }
+        }).then(function(_ref) {
+            var text = _ref.data.text;
+            g_form.setValue('u_ocr_text', text);
+        });
+    });
+
+}
